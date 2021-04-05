0976e53d394f45e4257fbdf15139564be1e63da8
BryceHamilton <bryce.m.hamilton@outlook.com>
2021-04-05 01:00:42 -0400

    revert to bare express server

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth-routes');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect(process.env.DB_URL || '', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('db connected'));
-
 express()
-  .use(cors({ origin: 'http://localhost:3000' }))
   .use(morgan('dev'))
   .use(express.urlencoded({ extended: true }))
   .use(express.json())
-
-  .use('/auth', authRoutes)
 
   .listen(PORT, () => {
     console.info(`🌍 Listening on port ${PORT}`);
